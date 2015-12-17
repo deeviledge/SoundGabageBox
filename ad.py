4bdd4e16d3a7a05449159000565744ad7ed7767d
okumura <ai.mychronicle07@gmail.com>
2015-12-17 09:39:36 +0900

    インデント修正

--- ad.py
+++ ad.py
@@ -87,9 +87,9 @@ while True:
             # sys.stdout.write(str(mem[ch] - value))
         if (mem[ch] - value > 500):
             #sys.stdout.write(str(ii)+"\n")
-            ii += 1
-            pygame.mixer.init()
-            sound_flg=random.randint(1,5)
+		    ii += 1
+		    pygame.mixer.init()
+		    sound_flg=random.randint(1,5)
             if sound_flg==1:
             	pygame.mixer.music.load("/home/pi/okumura/drop1.mp3")
             	pygame.mixer.music.set_volume(play_volume/100)
